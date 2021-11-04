9d3de1cab239232a4a5eaff262c12e5f6129e298
Matan Wiesner <matanwis@gmail.com>
2021-11-04 17:30:52 +0000

    q2 base parsing

--- maman11/q2/athlete.py
+++ maman11/q2/athlete.py
@@ -5,20 +5,19 @@ import sys
 
 class CalcLexer(Lexer):
     # Set of token names.   This is always required
-    tokens = { ID, NUMBER, PLUS, MINUS, TIMES,
-               DIVIDE, ASSIGN, LPAREN, RPAREN }
+    tokens = { ORDER_NUMBER,SPACE,T,TIME_VAL,A,A_VAL,C, D, DV}
     # String containing ignored characters between tokens
-    ignore = ' \t'
+    ignore = '\n'
     # Regular expression rules for tokens
-    ID      = r'[a-zA-Z_][a-zA-Z0-9_]*'
-    NUMBER  = r'\d+'
-    PLUS    = r'\+'
-    MINUS   = r'-'
-    TIMES   = r'\*'
-    DIVIDE  = r'/'
-    ASSIGN  = r'='
-    LPAREN  = r'\('
-    RPAREN  = r'\)'
+    ORDER_NUMBER = r'\[\d\]'
+    SPACE = r' '
+    T=      r'\<time\>'
+    TIME_VAL= r'\d\.\d\d'
+    A=r'\<athlete\>'
+    A_VAL=r'\"[a-zA-Z ]*\"'
+    C= r'\<country\>'
+    D = r'\<date\>'
+    DV= r'\d\d? (January|February|March|April|May|June|July|August|September|October|November|December) \d\d\d\d'
 if __name__ == '__main__':
     file_path = sys.argv[1] #todo: handle error
     with open(file_path, 'r') as file:
@@ -26,3 +25,4 @@ if __name__ == '__main__':
     lexer = CalcLexer()
     for tok in lexer.tokenize(data):
         print('type=%r, value=%r' % (tok.type, tok.value))
+
