ac26a7e60b820c3132be640e32935f323b516d0e
Matan Wiesner <matanwis@gmail.com>
2021-11-04 18:49:25 +0000

    m11q2 finish

--- maman11/q2/athlete.py
+++ maman11/q2/athlete.py
@@ -1,28 +1,87 @@
 
 from sly import Lexer
 import sys
+import re
 
-
-class CalcLexer(Lexer):
-    # Set of token names.   This is always required
-    tokens = { ORDER_NUMBER,SPACE,T,TIME_VAL,A,A_VAL,C, D, DV}
-    # String containing ignored characters between tokens
+class AtleteLexer(Lexer):
+    tokens = {TITLE, SERIAL_NUMBER,SPACE,TIME_FIELD_NAME,TIME_VALUE,
+              ATHLETE_FIELD_NAME,STRING_LITERAL,COUNTRY_FIELD_NAME, 
+              DATE_FIELD_NAME, DATE_VALUE}
     ignore = '\n'
     # Regular expression rules for tokens
-    ORDER_NUMBER = r'\[\d\]'
+    TITLE = r'World Record'
+    SERIAL_NUMBER = r'\[\d\]'
     SPACE = r' '
-    T=      r'\<time\>'
-    TIME_VAL= r'\d\.\d\d'
-    A=r'\<athlete\>'
-    A_VAL=r'\"[a-zA-Z ]*\"'
-    C= r'\<country\>'
-    D = r'\<date\>'
-    DV= r'\d\d? (January|February|March|April|May|June|July|August|September|October|November|December) \d\d\d\d'
-if __name__ == '__main__':
+    TIME_FIELD_NAME=      r'\<time\>'
+    TIME_VALUE= r'\d\.\d\d'
+    ATHLETE_FIELD_NAME=r'\<athlete\>'
+    STRING_LITERAL=r'\"[a-zA-Z ]*\"'
+    COUNTRY_FIELD_NAME= r'\<country\>'
+    DATE_FIELD_NAME = r'\<date\>'
+    DATE_VALUE= r'\d\d? (January|February|March|April|May|June|July|August|September|October|November|December) \d\d\d\d'
+
+class  ProcessedToken:
+    def __init__(self, token_type, lexeme, attributes):
+        self.token_type =  token_type
+        self.lexeme = lexeme
+        self.attributes =  attributes
+
+def get_text():
+    if len(sys.argv) != 2:
+        raise Execption('need exactly  one argument')
     file_path = sys.argv[1] #todo: handle error
     with open(file_path, 'r') as file:
-        data = file.read()
-    lexer = CalcLexer()
-    for tok in lexer.tokenize(data):
-        print('type=%r, value=%r' % (tok.type, tok.value))
+        return file.read()
 
+def get_sly_tokens(text):
+    return list(AtleteLexer().tokenize(text))
+
+def filter_out_space(sly_tokens):
+    return [t for t in sly_tokens if t.type  != 'SPACE']
+
+def get_attributes(sly_token):
+    if sly_token.type == 'SERIAL_NUMBER':
+      match = re.match(r'\[(\d)\]',sly_token.value)
+      number =  int(match.group(1))
+      return {"number" :  number}
+
+    if sly_token.type == 'STRING_LITERAL':
+      match = re.match(r'\"([a-zA-Z ]*)\"', sly_token.value)
+      content =  match.group(1)
+      return {"content" :  content}    
+
+    if sly_token.type == 'TIME_VALUE':
+      return {"value" :  float(sly_token.value)}
+
+    if sly_token.type == 'DATE_VALUE':
+        match = re.match(
+            r'(\d\d?) (January|February|March|April|May|June|July|August|September|October|November|December) (\d\d\d\d)',
+             sly_token.value)
+        day  = int(match.group(1))
+        month  = match.group(2)
+        year  = int(match.group(3))
+        
+        return {"day": day, "month":  month, "year":  year}
+
+
+    return  {}
+
+
+def get_processed_tokens(sly_tokens):
+    return [ProcessedToken(t.type, t.value, get_attributes(t))  for t in  sly_tokens]
+
+def print_processed_tokens(processed_tokens):
+    print("TOKEN\tLEXEME\tATTRIBUTE")
+    for processed_token in processed_tokens:
+        attributes_str = ','.join([f"{k}={v}" for k, v  in processed_token.attributes.items()])
+        print(f"{processed_token.token_type}\t{processed_token.lexeme}\t{attributes_str}")
+
+def main():
+    text = get_text()
+    sly_tokens = get_sly_tokens(text)
+    sly_tokens = filter_out_space(sly_tokens)
+    processed_tokens = get_processed_tokens(sly_tokens)
+    print_processed_tokens(processed_tokens)
+
+if __name__ == '__main__':
+    main()
