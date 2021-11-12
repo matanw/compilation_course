0f97e60e9fb93307559186776386ae8a10b0f8cd
Matan Wiesner <matanwis@gmail.com>
2021-11-12 13:33:29 +0000

    12.1 basic

--- maman12/q1/cla.py
+++ maman12/q1/cla.py
@@ -1,17 +1,23 @@
-from sly import Lexer
+import sly 
 
 import sys
+import os
+
+SIGNATURE = "Student Name: Matan Wiesner"
+
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
 
-class SlyLexerExtention(Lexer):
+class LexerHelper(sly.Lexer):
+    """This is an extention to sly.Lexer to parse  the CPL  langunage
+    This class *do not*  implement the interfacce in question"""
     # Set of token names.   This is always required
     tokens = { NUMBER, ID, BREAK, CASE, DEFAULT, ELSE, FLOAT,
-               IF, INPUT, INT, OUTPUT, SWITCH, WHILE
+               IF, INPUT, INT, OUTPUT, SWITCH, WHILE,
                PLUS, MINUS, TIMES, DIVIDE, ASSIGN,
-               EQ, LT, LE, GT, GE, NE ,AND, OR}
+               EQ, LT, LE, GT, GE, NE ,AND, OR,NOT, CAST}
 
 
     literals = { '(', ')', '{','}', ',', ':', ';'}
@@ -31,13 +37,15 @@ class SlyLexerExtention(Lexer):
     GE      = r'>='
     GT      = r'>'
     NE      = r'!='
-    AND      = r'!='
+    AND      = r'&&'
     OR      = r'\|\|'
+    NOT =   r'!'
 
     NUMBER = r'\d+(\.\d*)?'
+    CAST = r'(static_cast\<int\>|static_cast\<float\>)'
 
     # Identifiers and keywords
-    ID = r'[a-Za-z]([a-Za-z0-9])*'
+    ID = r'[A-Za-z]([A-Za-z0-9])*'
     ID['break'] = BREAK
     ID['case'] = CASE
     ID['default'] = DEFAULT
@@ -62,18 +70,63 @@ class SlyLexerExtention(Lexer):
         self.index += 1
 
 
-def get_text():
+def get_attributes(sly_token):
+    return {}
+
+class  ProcessedToken:
+    def __init__(self, token_type, lexeme, attributes):
+        self.token_type =  token_type
+        self.lexeme = lexeme
+        self.attributes =  attributes
+
+class Lexer():
+    """ This class is the classs that implement the interface that required in question"""
+
+    def __init__(self, text):
+        self.tokens_generator = LexerHelper().tokenize(text)
+
+    def get_next_token(self):
+        """This funcction return next valid token, or None in case of  end of output"""
+        try:
+            sly_token=next(self.tokens_generator)
+        except StopIteration:
+            return None
+        return ProcessedToken(sly_token.type, sly_token.value, get_attributes(sly_token)) 
+
+
+
+def get_input_file_path():
     if len(sys.argv) != 2:
         raise Exception('need exactly  one argument')
-    file_path = sys.argv[1] #todo: handle error
-    with open(file_path, 'r') as file:
+    return sys.argv[1] 
+
+def get_text(input_file_path):
+    with open(input_file_path, 'r') as file:
         return file.read()
 
+def get_out_file_path(input_file_path):
+    return os.path.splitext(input_file_path)[0] + '.tok'
+
+def write_output_file(out_file_path, lexer):
+    with open(out_file_path, 'w') as file:
+        file.write("TOKEN\tLEXEME\tATTRIBUTE\n")
+        token = lexer.get_next_token()
+        while token:
+            attributes_str = ','.join([f"{k}={v}" for k, v  in token.attributes.items()])
+            file.write(f"{token.token_type}\t{token.lexeme}\t{attributes_str}\n")
+            token = lexer.get_next_token()
+        file.write(SIGNATURE)
+
+
+
 def main():
-    lexer = SlyLexerExtention()
-    a=lexer.tokenize(get_text())
-    for x in a:
-        print(x)
+    input_file_path = get_input_file_path()
+    text = get_text(input_file_path)
+    lexer = Lexer(text)
+    out_file_path = get_out_file_path(input_file_path)
+    write_output_file(out_file_path, lexer)
+    eprint(SIGNATURE)
+
 
 if __name__ == '__main__':
     main()
