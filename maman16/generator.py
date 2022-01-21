2c1425b5ba88efadd4393ceaba7e328df4c26f2c
Matan Wiesner <matanwis@gmail.com>
2022-01-21 08:59:45 +0200

    add generators

---
+++ maman16/generator.py
@@ -0,0 +1,19 @@
+
+class Generator:
+  def __init__(self, prefix: str):
+    self.prefix = prefix
+    self.current = 0
+
+  def get_next(self):
+    self.current += 1
+    return f"{self.prefix}{self.current}"
+
+class LabelGenerator(Generator):
+
+  def __init__(self):
+    super().__init__("L")
+
+class TempVarGenerator(Generator):
+
+  def __init__(self):
+    super().__init__("t")
